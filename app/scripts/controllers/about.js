6f2ac6f3c56d0979f4901daa68b6d3db5d1a3435
Juan Muñoz <alexx44@users.noreply.github.com>
2016-11-08 16:49:03 -0300

    Instalacion de YEOMAN AGULAR

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coworkApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the coworkApp
+ */
+angular.module('coworkApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coworkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the coworkApp
+ */
+angular.module('coworkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
